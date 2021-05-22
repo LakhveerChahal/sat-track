df1d74861261e18bbc7c45adcea6f824e1c05853
= <=>
2021-05-22 17:36:01 +0530

    added production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+const apiBaseUrl = 'https://api.n2yo.com/rest/v1/satellite/';
+const apiKey = 'YC9DQJ-37AG2W-N5G4NY-4KM3';
+
 export const environment = {
-  production: true
+  production: false,
+  apiBaseUrl: apiBaseUrl,
+  apiKey: apiKey
 };
