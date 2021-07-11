af44644edb6388a1a97aeb6f614c3cbe4718e43a
= <=>
2021-07-11 15:53:35 +0530

    feature/header: implemented header and searching of satellites

--- dev/src/app/components/navbar/navbar.component.ts
+++ dev/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef, OnInit } from '@angular/core';
+import { debounceTime, switchMap, distinctUntilChanged } from "rxjs/operators";
+import { MapService } from 'src/app/services/map.service';
+import { SearchSatellite } from '@models/search-satellite-response.model';
+import { Subject, empty } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.less']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('searchEl', { static: false }) searchEl: ElementRef<HTMLInputElement>;
+  searchObs: Subject<string> = new Subject<string>();
+  searchResults: SearchSatellite[] = [];
 
-  constructor() { }
+  constructor(private mapService: MapService) {
+    // const dummyData = new SearchSatellite('Starlink', 'asada');
+    // this.searchResults.push(dummyData);
+    // this.searchResults.push(dummyData);
+    // this.searchResults.push(dummyData);
+  }
 
   ngOnInit(): void {
+    this.searchObs
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((searchValue: string) => {
+          if (searchValue) return this.mapService.getSatelliteByName(searchValue)
+
+          return empty();
+        })
+      )
+      .subscribe((res: { satellite: SearchSatellite[]}) => {
+        console.log(res);
+        this.searchResults = res.satellite;
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.searchEl.nativeElement.onkeyup = (ev: KeyboardEvent) => {
+      this.searchObs.next(this.searchEl.nativeElement.value);
+    }
   }
 
 }
--- dev/src/environments/environment.ts
+++ dev/src/environments/environment.ts
@@ -1,13 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// For n2yo API
 const apiBaseUrl = 'https://api.n2yo.com/rest/v1/satellite/';
 const apiKey = 'YC9DQJ-37AG2W-N5G4NY-4KM3';
+
+// For hasura API
+const hasuraSecretKey = 'R6jbMtmywtXzMGapRCwHXvb4D919KZrSsq3jz7RERjCi6jXv4XPcqQVcEShIrxZT';
+const hasuraBaseUrl = 'https://satrack.hasura.app/api/rest/';
 
 export const environment = {
   production: false,
   apiBaseUrl: apiBaseUrl,
-  apiKey: apiKey
+  apiKey: apiKey,
+  hasura: {
+    hasuraBaseUrl: hasuraBaseUrl,
+    hasuraSecretKey: hasuraSecretKey
+  }
 };
 
 
