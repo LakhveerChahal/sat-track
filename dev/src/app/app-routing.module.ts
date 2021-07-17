1cdb14f4bb902112b8c34589cafec1cae1eda752
= <=>
2021-07-17 14:54:13 +0530

    added searching functionality

--- dev/src/app/app-routing.module.ts
+++ dev/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EarthComponent } from '@features/earth/earth.component';
 
-const routes: Routes = [{ path: '', component: EarthComponent }];
+const routes: Routes = [
+  { path: '', component: EarthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
