7cbbfcc1d15ee4d1920ebcb2dce2b57ca4b52b76
= <=>
2021-11-10 11:05:49 +0530

    giving each orbit a random color

--- client/dev/src/app/models/satellite.model.ts
+++ client/dev/src/app/models/satellite.model.ts
@@ -1,9 +1,14 @@
+import * as Cesium from 'cesium';
+
 export class SatelliteModel {
+    public orbitColor: Cesium.Color;
     constructor(
         public satId: number,
         public satName: string,
         public isRendered: boolean,
         public isOrbitRendered: boolean,
         public tleData: string
-    ) {}
+    ) {
+        this.orbitColor = Cesium.Color.fromRandom();
+    }
 }
