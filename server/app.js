137809394c1198f0d0751515cd40e5d0837e355f
= <=>
2021-11-10 12:35:08 +0530

    serving static files from node js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(express.static(process.cwd() + "/public/"));
+
 app.get('/', (req, res) => {
-    res.send('Welcome');
+    res.sendFile(process.cwd() + "/public/index.html");
 });
 
 module.exports = app;
